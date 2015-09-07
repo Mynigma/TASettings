dc6ee28b6633f32c53272a5d96cb0a0e6b853ec9
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2015-09-07 08:56:47 +0200

    cleaned pods

--- Example/Pods/Target Support Files/Pods-TASettings_Example-TASettings/Pods-TASettings_Example-TASettings-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TASettings_Example_TASettings : NSObject
-@end
-@implementation PodsDummy_Pods_TASettings_Example_TASettings
-@end
--- Example/Pods/Target Support Files/Pods-TASettings_Example/Pods-TASettings_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TASettings_Example : NSObject
-@end
-@implementation PodsDummy_Pods_TASettings_Example
-@end
